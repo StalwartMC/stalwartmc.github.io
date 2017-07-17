6485447bf870dff759339e9a63409196bd6037ec
Scott Stevenson <sstevenson@stalwartmc.com>
2017-07-17 08:22:44 -0500

    Switching back to Ben

--- assets/contact-form/js/form-scripts.js
+++ assets/contact-form/js/form-scripts.js
@@ -20,7 +20,7 @@ function submitForm(){
 
     $.ajax({
         method: "POST",
-        url: "https://formspree.io/sstevenson@stalwartmc.com",
+        url: "https://formspree.io/barmenta@stalwartmc.com",
         dataType: "json",
         data: {name: name, _replyto: email, message: message, _gotcha: gotcha, _subject: "Stalwartmc.com contact form"},
         success : function(data, status, jq){
