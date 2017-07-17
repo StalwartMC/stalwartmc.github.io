e73a96d54e2ffbe110d391630cf4132ac1a77b6a
Scott Stevenson <sstevenson@stalwartmc.com>
2017-07-17 12:53:58 -0500

    Fixing map marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -22,24 +22,25 @@ function init() {
     var mapElement = document.getElementById('d');
     var map = new google.maps.Map(mapElement, mapOptions);
     var locations = [
-['Apple store', 'undefined', 'undefined', 'undefined', 'undefined', 29.7428417, -95.7784113, 'https://mapbuildr.com/assets/img/markers/default.png']
+['StalwartMC', 'undefined', 'undefined', 'undefined', 'undefined', 29.7428417, -95.7784113, 'assets/images/default.png']
     ];
     for (i = 0; i < locations.length; i++) {
-  if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-  if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-  if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-       if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-       if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-        marker = new google.maps.Marker({
-            icon: markericon,
-            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-            map: map,
-            title: locations[i][0],
-            desc: description,
-            tel: telephone,
-            email: email,
-            web: web
-        });
-link = '';     }
+      if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+      if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+      if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+      if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+      if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+      marker = new google.maps.Marker({
+          icon: markericon,
+          position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+          map: map,
+          title: locations[i][0],
+          desc: description,
+          tel: telephone,
+          email: email,
+          web: web
+      });
+      link = '';
+    }
 
 }
